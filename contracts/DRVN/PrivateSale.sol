0c668933d7f8323e047d69bb02a1cb0fd445c51f
rad$$ <105228117+radss000@users.noreply.github.com>
2023-05-04 20:25:00 +0200

    Update PrivateSale.sol

--- contracts/DRVN/PrivateSale.sol
+++ contracts/DRVN/PrivateSale.sol
@@ -1,43 +1,71 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./DRVNVesting.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/TokenTimelock.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./VestingContract.sol";
 
+/***
+ * @title Drivn
+ * @author radss000
+ */
+
+contract TokenTimeLockFactory is Ownable {
+    function createTimeLock(IERC20 token, address beneficiaryAddress, uint256 releaseTime, uint256 amount) external onlyOwner returns (TokenTimelock) {
+        TokenTimelock lock = new TokenTimelock(token, beneficiaryAddress, releaseTime);
+        token.transfer(address(lock), amount);
+        return lock;
+    }
+}
 contract PrivateSales is Ownable {
+    using SafeERC20 for IERC20;
 
     // DRVN token
     IERC20 public immutable token;
 
+    // coin price
+    uint256 public coinPrice;
+
     // private sale enable flag
     bool public privateSalesEnabled;
 
-    // mapping between address and its created VestingContracts contracts
+    // Vesting contract instance
+    VestingContract public vesting;
+
+    // mapping between address and its created VestingContracts 
     mapping(address => address[]) public vestingWallets;
 
     // mapping for allowed addresses
     mapping(address => bool) public isAllowed;
 
-    // price of one DRVN token in wei
-    uint256 public coinPrice;
+    // Events
+    event TokenPurchase(address indexed buyer, uint256 amount, uint256 releaseTime);
+    event CoinPriceUpdated(uint256 newCoinPrice);
 
-    constructor(address token_, uint256 initialCoinPrice) {
-        token = IERC20(token_);
+    constructor(IERC20 token_, uint256 initialCoinPrice) {
+        token = token_;
         coinPrice = initialCoinPrice;
     }
 
-    /**
+    function updateCoinPrice(uint256 newCoinPrice) external onlyOwner {
+        coinPrice = newCoinPrice;
+        emit CoinPriceUpdated(newCoinPrice);
+    }
+    /** 
      * @dev setting privateSalesEnabled variable
-     * @param enabled boolean True if enabled, False otherwise
-     */
+     * @param enabled boolean True if enables, False otherwise
+    */
     function setPrivateSalesEnabled(bool enabled) external onlyOwner {
         privateSalesEnabled = enabled;
     }
 
-    /**
+    /** 
      * @dev setting allowed addresses
-     * @param allowed boolean True if enabled, False otherwise
-     */
+     * @param allowed boolean True if enables, False otherwise
+    */
     function setAllowed(address[] calldata addresses, bool allowed) external onlyOwner {
         for (uint256 i = 0; i < addresses.length; i++) {
             isAllowed[addresses[i]] = allowed;
@@ -52,66 +80,105 @@ contract PrivateSales is Ownable {
         _;
     }
 
-    /**
-     * @dev createVestWallet function will create the VestWallet
-     * @param beneficiaryAddress is the address of the beneficiary
-     * @param durationSeconds is the duration of vesting in seconds after it starts
-     * @param amount is the amount of coins
-     */
-    function _createVestWallet(
-        address beneficiaryAddress,
-        uint64 durationSeconds,
-        uint256 amount
-    ) internal {
-        // creating vesting wallet contract
-        VestingContract vestingWallet = new VestingContract(
-            beneficiaryAddress, 
-            block.timestamp + 360 days,
-            durationSeconds, 
-            address(token)
-        );
 
-        // transfer vesting amount to VestingWallet contract
-        token.transfer(address(vestingWallet), amount);
-
-        // adding created contract in this mapping
-        vestingWallets[beneficiaryAddress].push(address(vestingWallet));
-    }
+    event Withdrawn(address indexed owner, uint256 amount);
 
     /**
-     * @dev function gets a created VestWallet contracts for provided account
-     * @param account is the account address
-     */
-    function getAccountVestingWallets(address account) external view returns (address[] memory) {
-        return vestingWallets[account];
-    }
-
-    /**
-     * @dev Buy coins in private coin supply
-     */
-    function buy() external payable whenAllowed {
-        require(privateSalesEnabled, "PrivateSales: sale is not enabled");
-        require(msg.value > 0, "PrivateSales: should not be zero amount");
-
-        uint256 amount = msg.value * 10 ** 18 / coinPrice;
-        _createVestWallet(msg.sender, 360 days, amount);
-    }
-
-    /**
-     * @dev Update the coin price
-     * @param newCoinPrice The new coin price in wei
-     */
-    function setCoinPrice(uint256 newCoinPrice) external onlyOwner {
-        require(newCoinPrice > 0, "PrivateSales: coin price should be greater than 0");
-        coinPrice = newCoinPrice;
-    }
-
-    /**
-     * @dev Withdraw the amount of coins from contract
+     * @dev Withdraw the amount of coins from contract address to owner
      */
     function withdraw() external onlyOwner {
-        (bool success,) = payable(owner()).call{value : address(this).balance}("");
-        require(success, "PrivateSales: unsuccessful withdraw");
+        uint256 balance = token.balanceOf(address(this));
+        require(balance > 0, "PrivateSales: contract has no balance");
+
+        token.safeTransfer(owner(), balance);
+
+        emit Withdrawn(owner(), balance);
     }
+
+    function getContractTokenBalance() external view returns (uint256) {
+    return token.balanceOf(address(this));
 }
 
+    function getContractBalance() external view returns (uint256) {
+    return address(this).balance;
+}
+
+    event TokensBought(address indexed buyer, uint256 amount, uint256 price);
+
+    //**
+ //* @dev Buy the coins in private Coins supply
+ //
+    function buy(uint256 amount) external payable whenAllowed {
+        require(privateSalesEnabled, "Private sales is not enabled");
+        require(amount > 0, "Amount must be greater than 0");
+
+        uint256 tokenPrice;
+        uint256 tokensSold;
+        uint256 remainingEth;
+
+        uint256 cliff;
+        uint256 vestingDuration;
+        uint256 initialAmountPercentage;
+        uint256 releaseTime;
+
+        if (amount <= 7000000) {
+            // Private Sale 1
+            tokenPrice = 27000000000000; // $0.045
+            tokensSold = amount;
+
+            cliff = 8 * 30 days; // cliff period of 8 months
+            vestingDuration = 10 * 30 days; // vesting period of 10 months
+            initialAmountPercentage = 10; // TGE => 10% of the allocation
+        } else if (amount <= 13000000) {
+            // Private Sale 2
+            tokenPrice = 45000000000000; // $0.075
+            tokensSold = amount - 7000000;
+
+            cliff = 4 * 30 days; // cliff period of 4 months
+            vestingDuration = 8 * 30 days; // vesting period of 8 months
+            initialAmountPercentage = 15; // TGE => 15% of the allocation
+        } else {
+            revert("PrivateSales: amount exceeds total number of tokens available for private sale");
+        }
+
+        uint256 price = tokenPrice * tokensSold;
+
+        require(msg.value >= price, "Insufficient Ether sent");
+
+        // transfer the tokens to the buyer
+        token.safeTransfer(msg.sender, tokensSold);
+
+        // calculate the remaining Ether, and transfer it to the owner of the contract
+        remainingEth = msg.value - price;
+        if (remainingEth > 0) {
+            payable(owner()).transfer(remainingEth);
+        }
+
+        // create a token time lock for the buyers tokens
+        uint256 initialAmount = (tokensSold * initialAmountPercentage) / 100;
+        uint256 remainingAmount = tokensSold - initialAmount;
+        releaseTime = block.timestamp + cliff;
+
+        // 1st day of the month at 00:00 UTC
+        uint256 monthInSeconds = 30 days;
+        releaseTime = (releaseTime / monthInSeconds) * monthInSeconds;
+
+        TokenTimeLockFactory factory = new TokenTimeLockFactory();
+        TokenTimelock initialLock = factory.createTimeLock(token, msg.sender, releaseTime, initialAmount);
+
+        vestingWallets[msg.sender].push(address(initialLock));
+        
+        uint256 linearUnlock = remainingAmount / vestingDuration;
+        for (uint256 i = 1; i <= vestingDuration / monthInSeconds; i++) {
+            uint256 unlockTime = releaseTime + (i * monthInSeconds);
+            factory.createTimeLock(token, msg.sender, unlockTime, linearUnlock);
+        }
+
+        emit TokensBought(msg.sender, tokensSold, price);
+    }
+
+        function getVestingWallets(address walletAddress) external view returns (address[] memory) {
+            return vestingWallets[walletAddress];
+        }
+
+    }
