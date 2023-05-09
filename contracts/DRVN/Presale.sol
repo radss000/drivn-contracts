bd2f536288718ef80563dcf7ce0ffad1f830cb39
rad$$ <105228117+radss000@users.noreply.github.com>
2023-05-09 21:03:18 +0200

    Update Presale.sol

--- contracts/DRVN/Presale.sol
+++ contracts/DRVN/Presale.sol
@@ -5,6 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/TokenTimelock.sol";
 
+/***
+ * @title Drivn
+ * @author radss000
+ */
+
 contract TokenTimeLockFactory is Ownable {
     function createTimeLock(IERC20 token, address beneficiaryAddress, uint256 releaseTime, uint256 amount) external onlyOwner {
         TokenTimelock lock = new TokenTimelock(token, beneficiaryAddress, releaseTime);
@@ -37,9 +42,8 @@ contract PreSales is Ownable {
         coinPrice = initialCoinPrice;
     }
 
-    function enablePreSales() external onlyOwner {
-        _setAllowed(0x0A0f423b26AdE420c960c8Ca9a30170afc5f4001, true);
-        preSalesEnabled = true;
+    function setPreSalesEnabled(bool enabled) external onlyOwner {
+        preSalesEnabled = enabled;
     }
 
 
@@ -70,36 +74,45 @@ contract PreSales is Ownable {
         uint256 releaseTime,
         uint256 amount
     ) internal {
-        TokenTimelock lock = new TokenTimelock(token, beneficiaryAddress, releaseTime);
-        token.transfer(address(lock), amount);
-        lockContracts[beneficiaryAddress].push(address(lock));
+        for (uint i = 0; i < 12; i++) {
+            uint256 installmentReleaseTime = releaseTime + i * 30 days;
+            TokenTimelock lock = new TokenTimelock(token, beneficiaryAddress, installmentReleaseTime);
+            token.transfer(address(lock), amount / 12);
+            lockContracts[beneficiaryAddress].push(address(lock));
+        }
     }
 
     function getAccountLockContracts(address account) external view returns (address[] memory) {
         return lockContracts[account];
     }
 
-    function buy() external payable {
-    require(preSalesEnabled, "PreSales: sale is not enabled");
-    require(allowed[msg.sender], "PreSales: address not allowed");
-    require(msg.value > 0, "PreSales: should not be zero amount");
-
-    uint256 amount = (msg.value * coinPrice) / 10**18;
-
-    require(token.balanceOf(address(this)) >= amount, "PreSales: insufficient balance");
-
-    uint256 halfAmount = amount / 2;
-    uint256 remainingAmount = amount - halfAmount;
-
-    require(token.transferFrom(msg.sender, address(this), halfAmount), "PreSales: token transfer failed");
-
-    // Remaining tokens released after 360 days
-    uint256 releaseTime = block.timestamp + 360 days;
-    _createTimeLock(msg.sender, releaseTime, remainingAmount);
-
-    emit TokenPurchase(msg.sender, amount, releaseTime);
+    function getContractTokenBalance() external view returns (uint256) {
+    return token.balanceOf(address(this));
 }
 
+    function buy() external payable {
+        require(preSalesEnabled, "PreSales: sale is not enabled");
+        require(allowed[msg.sender], "PreSales: address not allowed");
+        require(msg.value > 0, "PreSales: should not be zero amount");
+
+        uint256 amount = (msg.value * coinPrice) / 10**18;
+        uint256 maxAmount = 9000000; // max nb of tokens that can be sold in seed sale 
+        require(amount <= maxAmount, "PreSales: amount exceeds maximum number of tokens that can be sold in the Seed sale");
+
+        require(token.transferFrom(msg.sender, address(this), amount), "PreSales: token transfer failed");
+
+        // 5% tokens released immediately (TGE)
+        uint256 immediateReleaseAmount = (amount * 5) / 100;
+        token.transfer(msg.sender, immediateReleaseAmount);
+
+        // 95% tokens released linearly after the cliff
+        uint256 cliff = 10 * 30 days; // 10 months for the cliff period 
+        uint256 remainingAmount = (amount * 95) / 100;
+        uint256 releaseTime = block.timestamp + cliff;
+        _createTimeLock(msg.sender, releaseTime, remainingAmount);
+
+        emit TokenPurchase(msg.sender, amount, releaseTime);
+    }
 
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
