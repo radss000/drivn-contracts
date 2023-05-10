d4e30dced9d96782da5779c078c42af29a6576b5
rad$$ <105228117+radss000@users.noreply.github.com>
2023-05-10 13:55:29 +0200

    Update IEarnNFT.sol

--- contracts/EarnNFT/IEarnNFT.sol
+++ contracts/EarnNFT/IEarnNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.18;
 
 /**
     interface for EarnNFT
@@ -24,5 +24,8 @@ interface IEarnNFT {
      * @param tokenId id of token
     */
     function ownerOf(uint256 tokenId) external view returns(address);
+
+
+    function level(uint256 tokenId) external view returns(uint256);
 
 }
