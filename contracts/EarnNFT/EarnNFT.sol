41eea860415858866897461e5f6ad87cec2e9e1b
rad$$ <105228117+radss000@users.noreply.github.com>
2023-05-10 14:57:51 +0200

    Update EarnNFT.sol

--- contracts/EarnNFT/EarnNFT.sol
+++ contracts/EarnNFT/EarnNFT.sol
@@ -1,44 +1,52 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.18;
 
-
-import "C:\Users\radia\node_modules\@openzeppelin\contracts-upgradeable\token\ERC721\extensions\ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./IEarnNFTManagement.sol";
 
-contract EarnNFT is ERC721Enumerable, Ownable {
+interface IEarnNFT {
+    function mint(address account) external returns (uint256);
+    function burn(uint256 tokenId) external;
+    function setAllowed(address allowedAddress, bool allowed) external;
+    function level(uint256 tokenId) external view returns (uint256);
+}
+
+contract EarnNFT is ERC721Enumerable, Ownable, IEarnNFT {
     using Counters for Counters.Counter;
 
     // token counter
     Counters.Counter public tokenIdCounter;
 
     // mapping for allowed addresses
-    mapping(address=>bool) public isAllowed;
+    mapping(address => bool) public isAllowed;
 
     // base token URI
     string public baseTokenURI;
+
+    IEarnNFTManagement public earnNFTManagement;
 
     /**
      * @dev Sets main dependencies and constants
      * @param name_ 721A nft name
      * @param symbol_ 721A nft symbol
      * @param baseURI baseUri for mint
-    */
-
+     * @param earnNFTManagement_ address of EarnNFTManagement contract
+     */
     constructor(
-        string memory name_, 
-        string memory symbol_, 
-        string memory baseURI
-    ) 
-    ERC721(name_, symbol_){
+        string memory name_,
+        string memory symbol_,
+        string memory baseURI,
+        address earnNFTManagement_
+    ) ERC721(name_, symbol_) {
         setBaseURI(baseURI);
+        earnNFTManagement = IEarnNFTManagement(earnNFTManagement_);
     }
-
 
     /**
      * @dev modifier to detect if address is allowed for specific operation
-    */
-
+     */
     modifier whenAllowed() {
         require(isAllowed[msg.sender], "EarnNFT: address is not allowed to call this function");
         _;
@@ -46,9 +54,8 @@ contract EarnNFT is ERC721Enumerable, Ownable {
 
     /**
      * @dev minting the token on certain address
-    */
-
-    function mint(address account) external whenAllowed returns (uint256) {
+     */
+    function mint(address account) external override whenAllowed returns (uint256) {
         tokenIdCounter.increment();
         uint256 tokenId = tokenIdCounter.current();
         _mint(account, tokenId);
@@ -58,39 +65,37 @@ contract EarnNFT is ERC721Enumerable, Ownable {
     /**
      * @dev burning the token
      * @param tokenId id of token
-    */
-
-    function burn(uint256 tokenId) external whenAllowed {
+     */
+    function burn(uint256 tokenId) external override whenAllowed {
         _burn(tokenId);
     }
-
 
     /**
      * @dev setting allowed addresses for nft usage
      * @param allowedAddress allowed address
      * @param allowed True/False bool for enable certain operations
-    */
-    
-    function setAllowed(address allowedAddress, bool allowed) external onlyOwner {
+     */
+    function setAllowed(address allowedAddress, bool allowed) external override onlyOwner {
         isAllowed[allowedAddress] = allowed;
     }
-
 
     /**
      * @dev Set the base URI
      * @param baseURI_ Base path to metadata
-    */
-
-    function setBaseURI(string memory baseURI_) public onlyOwner {
+     */
+    function setBaseURI(string memory baseURI_) public virtual onlyOwner {
         baseTokenURI = baseURI_;
     }
 
     /**
-     * @dev Get current base uri 
-    */
+     * @dev Get current base uri
+     */
+    function level(uint256 tokenId) external view override returns (uint256) {
+        IEarnNFTManagement.NFTInformation memory nftInfo = earnNFTManagement.nftInfo(tokenId);
+        return uint256(nftInfo.nftType);
+    }
 
     function _baseURI() internal view override returns (string memory) {
         return baseTokenURI;
     }
-
 }
